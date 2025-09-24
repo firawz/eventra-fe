52c536986107c2f642f1c94a81feaf3280d032c1
Danuwindu <128960391+gilangdanuwinduif@users.noreply.github.com>
2025-09-25 00:18:15 +0700

    Resolve all

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -23,35 +22,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </Provider>
   </React.StrictMode>
 );
-=======
-import React from 'react'
-import './App.css'
-import App from './App'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route
-          path="/about"
-          element={
-            <>
-              <div className="text-center">
-                <h1 className="text-xl">About</h1>
-                <div>
-                  <Link to="/" className="text-purple-400 underline">
-                    Home
-                  </Link>
-                </div>
-              </div>
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-)
->>>>>>> cdbb2de09f559555709fbc1ad8eb7e629cda606c
